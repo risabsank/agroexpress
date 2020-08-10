20039a77585603cb0b548b6aa121ea4a9027725a
RisabS <risab.sankar@gmail.com>
2020-08-10 13:13:49 -0700

    Buyer and Farmer Registration Completed

--- lib/farmreg2.dart
+++ lib/farmreg2.dart
@@ -1,3 +1,5 @@
+import 'package:agroexpress/farmreg1.dart';
+import 'package:agroexpress/farmreg3.dart';
 import 'package:flutter/material.dart';
 
 class FarmRegTwo extends StatefulWidget {
@@ -6,8 +8,260 @@ class FarmRegTwo extends StatefulWidget {
 }
 
 class _FarmRegTwoState extends State<FarmRegTwo> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/farmreg2.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 100.0,
+              ),
+              Text(
+                "Almost Set!",
+                style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10.0),
+                child: Text(
+                  "Fill out the boxes below and",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 10.0),
+                child: Text(
+                  "your one step closer!",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 15.0),
+                child: TextFormField(
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: 'Street Address',
+                    contentPadding: EdgeInsets.only(left: 20.0),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5.0),
+                      child: ImageIcon(
+                        AssetImage('assets/formicon.png'),
+                        color: Colors.green,
+                      ),
+                    ),
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'No Response Has Been Entered';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 15.0),
+                child: TextFormField(
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: 'City',
+                    contentPadding: EdgeInsets.only(left: 20.0),
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5.0),
+                      child: ImageIcon(
+                        AssetImage('assets/formicon.png'),
+                        color: Colors.green,
+                      ),
+                    ),
+                    border: InputBorder.none,
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'No Response Has Been Entered';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 15.0),
+                child: TextFormField(
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: 'State',
+                    contentPadding: EdgeInsets.only(left: 20.0),
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5.0),
+                      child: ImageIcon(
+                        AssetImage('assets/formicon.png'),
+                        color: Colors.green,
+                      ),
+                    ),
+                    border: InputBorder.none,
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'No Response Has Been Entered';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white),
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                margin: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 15.0),
+                child: TextFormField(
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: 'Zipcode',
+                    contentPadding: EdgeInsets.only(left: 20.0),
+                    labelStyle: TextStyle(
+                      color: Colors.white,
+                    ),
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5.0),
+                      child: ImageIcon(
+                        AssetImage('assets/formicon.png'),
+                        color: Colors.green,
+                      ),
+                    ),
+                    border: InputBorder.none,
+                  ),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'No Response Has Been Entered';
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 95.0,
+              ),
+              Row(
+                children: <Widget> [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.black12,
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(50.0)
+                    ),
+                    margin: EdgeInsets.fromLTRB(10.0, 0.0, 50.0, 0.0),
+                    child: FlatButton(
+                      onPressed: ()  => Navigator.push(context, new MaterialPageRoute(builder: (_) => FarmRegOne())),
+                      child: Text(
+                        "Go Back",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: Image.asset("assets/agroicon.png", scale: 14),
+                      ),
+                      Text(
+                        "AGRO Express",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.black12,
+                        border: Border.all(color: Colors.white),
+                        borderRadius: BorderRadius.circular(50.0)
+                    ),
+                    margin: EdgeInsets.fromLTRB(55.0, 0.0, 10.0, 0.0),
+                    child: FlatButton(
+                      onPressed: ()  => Navigator.push(context, new MaterialPageRoute(builder: (_) => FarmRegThree())),
+                      child: Text(
+                        "Next",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/termofservice.dart
@@ -0,0 +1,205 @@
+import 'package:agroexpress/farmreg3.dart';
+import 'package:agroexpress/login.dart';
+import 'package:flutter/material.dart';
+
+class TermsService extends StatefulWidget {
+  @override
+  _TermsServiceState createState() => _TermsServiceState();
+}
+
+class _TermsServiceState extends State<TermsService> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/terms.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 50.0,
+            ),
+          Text(
+            "Welcome!",
+            style: TextStyle(
+              fontSize: 30.0,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Text(
+              "Read the Terms of Service",
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Text(
+              "and Community Guidelines!",
+              style: TextStyle(
+                fontSize: 15.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 27.0),
+                child: FlatButton(
+                  onPressed: () {},
+                    child: Text(
+                      'Click to Read Terms of Service',
+                      style: TextStyle(
+                        color: Colors.white60,
+                        fontSize: 15.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+              padding: const EdgeInsets.symmetric(horizontal: 5.0),
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 5.0),
+                child: FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Click to Read Community Guidelines',
+                    style: TextStyle(
+                      color: Colors.white60,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 10.0),
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                border: Border.all(color: Colors.white),
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Row(
+                children: [
+                  Image.asset("assets/formicon.png", scale: 2),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        "By clicking Create Account, you have accepted and agreed ",
+                            style: TextStyle(
+                            color: Colors.white,
+                              fontSize: 10.0,
+                      ),
+                      ),
+                      Text(
+                        "to the ToS and Community Guidelines. ",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 100.0,
+            ),
+            Row(
+              children: <Widget> [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.black12,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(50.0)
+                  ),
+                  margin: EdgeInsets.fromLTRB(10.0, 0.0, 50.0, 0.0),
+                  child: FlatButton(
+                    onPressed: ()  => Navigator.push(context, new MaterialPageRoute(builder: (_) => FarmRegThree())),
+                    child: Text(
+                      "Go Back",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: Image.asset("assets/agroicon.png", scale: 14),
+                    ),
+                    Text(
+                      "AGRO Express",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 10.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.black12,
+                      border: Border.all(color: Colors.white),
+                      borderRadius: BorderRadius.circular(50.0)
+                  ),
+                  margin: EdgeInsets.fromLTRB(22.0, 0.0, 10.0, 0.0),
+                  child: FlatButton(
+                    onPressed: ()  => Navigator.push(context, new MaterialPageRoute(builder: (_) => LoginScreen())),
+                    child: Text(
+                      "Create Account",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
